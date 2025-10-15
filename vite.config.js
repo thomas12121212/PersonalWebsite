c0c657380327a1931b1af7bb97179f622f58a506
watchman <th376961@dal.ca>
2025-10-15 19:41:26 -0300

    testing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PersonalWebsite/', // 👈 must exactly match your GitHub repo name (case-sensitive)
+  base: '/PersonalWebsite/',
 })
