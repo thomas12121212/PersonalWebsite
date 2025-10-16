2ae89de2e3be219a85ac890466bc76ffd1ca57e0
watchman <th376961@dal.ca>
2025-10-16 13:30:38 -0300

    fixed navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,23 @@
 export default function Home() {
   return (
       <>
-          <h1></h1>
-          <div className="paragraph">
-          <p>Hi,
-              <br></br> My name is Thomas Watchman.
-              <br></br> Thanks for visiting
-              <br></br> Connect with me here
-
-          </p>
-          </div>
-          <h2 className="section-title">Explore</h2>
-          <div className="paragraph">
-          <p>
-              Read <a href="#">what I’m working on</a>, browse <a href="#">projects</a>,
-              or check the <a href="#">archive</a>.
-          </p>
-          </div>
-          <h2 className="section-title">Connect</h2>
-          <div className="icon-row">
-              <a href="mailto:thomas@example.com">Email</a>
-              <a href="https://github.com/thomas12121212" target="_blank" rel="noopener noreferrer">GitHub</a>
-              <a href="https://www.linkedin.com/in/thomaswatchman" target="_blank"
-                 rel="noopener noreferrer">LinkedIn</a>
-          </div>
-          <div>
-              CS student @ Dal
-          </div>
+          <main>
+              <div className="paragraph">
+                  <p>Hi,
+                      <br></br> My name is Thomas Watchman.
+                      <br></br> Thanks for visiting
+                      <br></br> Connect with me here
+                      Read <a href="#">what I’m working on</a>, browse <a href="#">projects</a>,
+                      or check the <a href="#">archive</a>.
+                  </p>
+                  <div className="icon-row">
+                      <a href="mailto:thomas@example.com">Email</a>
+                      <a href="https://github.com/thomas12121212" target="_blank" rel="noopener noreferrer">GitHub</a>
+                      <a href="https://www.linkedin.com/in/thomaswatchman" target="_blank"
+                         rel="noopener noreferrer">LinkedIn</a>
+                  </div>
+              </div>
+          </main>
       </>
   )
 }
