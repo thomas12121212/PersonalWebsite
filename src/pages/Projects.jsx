f930073c0dd5a398ca4912af555606841658bb0e
watchman <th376961@dal.ca>
2025-11-27 10:20:09 -0400

    Fixed text formatting on projects and quickcash page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom'
 export default function Projects() {
   return (
     <>
-      <div className="view">
-        <h1>Projects</h1>
-          <ul>
-              <li><strong>Personal Website</strong> — Vite + React on GitHub Pages.</li>
-              <li><strong>Portfolio Visualizer</strong> — My Portfolio broken down{' '}<Link to="/projects/tfsa">Open</Link></li>
-              <li><strong>Quick Cash App</strong> — A marketplace style app for managing jobs{' '}<Link to="/projects/quickcash">Open</Link></li>
-          </ul>
-      </div>
+        <div className="view">
+            <h1>Projects</h1>
+            <div className="projects">
+                <ul>
+                    <li><strong>Personal Website</strong> — Vite + React on GitHub Pages.</li>
+                    <li><strong>Portfolio Visualizer</strong> — My Portfolio broken down{' '}<Link
+                        to="/projects/tfsa">Open</Link></li>
+                    <li><strong>Quick Cash App</strong> — A marketplace app for managing jobs{' '}<Link
+                        to="/projects/quickcash">Open</Link></li>
+                </ul>
+            </div>
+        </div>
     </>
   )
 }
